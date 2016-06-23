ae1e665675e7106ea39754c17c615ca40a7d7fa2
Nikolay Kagala <nikolaykagala@macbook-air-nikolay.local>
2016-06-23 22:00:28 +0600

    Initial Commit

---
+++ NKDownloadControl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NKDownloadControl
+//
+//  Created by Nikolay Kagala on 23/06/16.
+//  Copyright © 2016 Nikolay Kagala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NKDownloadControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NKDownloadControl
+//
+//  Created by Nikolay Kagala on 23/06/16.
+//  Copyright © 2016 Nikolay Kagala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
